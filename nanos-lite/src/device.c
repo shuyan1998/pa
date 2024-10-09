6af7b45ae61ca47e8a7545f3f0581b40db68a7bd
shuwang <shuwang@ubuntu22.04.myguest.virtualbox.org>
2024-10-09 18:34:05 +0800

    [pa3]Add support for getting screen size(vga) in ndl lib.

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -27,13 +27,12 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   if(ev.keycode == AM_KEY_NONE){
     return 0;
   }
-  snprintf((char*)buf, len, "%s %s", ev.keydown? "kd": "ku", keyname[ev.keycode]);
-  
-  return len;
+  return snprintf((char*)buf, len, "%s %s", ev.keydown? "kd": "ku", keyname[ev.keycode]);
 }
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  return 0;
+  AM_GPU_CONFIG_T config = io_read(AM_GPU_CONFIG);
+  return snprintf((char*)buf, len, "WIDTH:%d\nHEIGHT:%d\n", config.width, config.height);
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -4,10 +4,14 @@
 #include <string.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <assert.h>
+#define buf_size 1024
 
 static int evtdev = -1;
 static int fbdev = -1;
 static int screen_w = 0, screen_h = 0;
+static int canvas_w = 0, canvas_h = 0;
+static int canvas_x = 0, canvas_y = 0;
 
 uint32_t NDL_GetTicks() {
   struct timeval now;
@@ -42,6 +46,52 @@ void NDL_OpenCanvas(int *w, int *h) {
     }
     close(fbctl);
   }
+  
+  // read from /dev/dispinfo to get screen width and height
+  char *buf = (char*)malloc(buf_size * sizeof(char));
+  int fd = open("/proc/dispinfo", 0, 0);
+  int ret = read(fd, buf, buf_size);
+  assert(ret < buf_size);
+  assert(close(fd) == 0);
+  
+  int i = 0;
+  int width = 0, height = 0;
+  /// check 'WIDTH'
+  assert(strncmp(&buf[i], "WIDTH:", 6) == 0);
+  i += 6;
+  for(;i < buf_size; i++) {
+    if(buf[i] >= '0' && buf[i] <= '9') {
+      width = width * 10 + buf[i] - '0';
+    }
+    else {
+      break;
+    }
+  }
+  assert(buf[i++] == '\n');
+  // check 'HEIGHT'
+  assert(strncmp(buf + i, "HEIGHT:", 7) == 0);
+  i += 7;
+  for(;i < buf_size; i++) {
+    if(buf[i] >= '0' && buf[i] <= '9') {
+      height = height * 10 + buf[i] - '0';
+    }
+    else {
+      break;
+    }
+  }
+
+  free(buf);
+  screen_w = width; screen_h = height;
+  if(*w == 0 && *h == 0) {
+    *w = width; *h = height;
+  }
+  canvas_w = *w;canvas_h = *h;
+  // set canvas position to centor of screen
+  canvas_x = (screen_w - canvas_w) / 2;
+  canvas_y = (screen_h - canvas_h) / 2;
+  printf("screen_w: %d, screen_h: %d\n", screen_w, screen_h);
+  printf("canvas_x: %d, canvas_y: %d\n", canvas_x, canvas_y);
+  
 }
 
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
